05d905c73d91915abe1ef0954d8a9de3946ea16b
Hong Pei Geok <120556342+peiyu00@users.noreply.github.com>
2023-06-27 10:02:22 +0800

    Delete submission/peiyu00/question1/files/code/tweets directory

--- submission/peiyu00/question1/files/code/tweets/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TweetsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tweets'
--- submission/peiyu00/question1/files/code/tweets/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from tweets.views import tweets_view
-
-urlpatterns = [
-    path('tweets/', tweets_view, name='tweets'),
-]
